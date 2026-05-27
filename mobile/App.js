d153be30526bffe71523eccbf54b7bf60ebe1723
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-05-27 10:44:39 -0300

    Create App.js

---
+++ mobile/App.js
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#050816',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text
+        style={{
+          color: '#ff7b00',
+          fontSize: 28,
+          fontWeight: 'bold'
+        }}
+      >
+        AstraLink Mobile
+      </Text>
+    </View>
+  )
+}
