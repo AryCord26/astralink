6ee39412445193600f1b8dd2a4df2e3708a66bec
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-05-27 10:18:11 -0300

    Create authMiddleware.js

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+
+export default function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({
+      error: 'Token not provided'
+    })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    )
+
+    req.userId = decoded.id
+
+    return next()
+  } catch {
+    return res.status(401).json({
+      error: 'Invalid token'
+    })
+  }
+}
